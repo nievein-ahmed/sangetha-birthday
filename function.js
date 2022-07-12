e330a423c7811dfe1e663861ae87469342cab441
nievein ahmed <nevenahmed039@gmail.com>
2022-07-12 17:01:01 +0200

    updating

---
+++ function.js
@@ -0,0 +1,16 @@
+//alert("TOUCH THE CAKE 🎂 AND ENJOY THE PARTY, 🎶🎉🎊");
+const  audio = document.getElementById("HBD")
+
+//window.addEventListener("click", function() {
+//audio.play()
+
+
+
+//});
+function playAudio() { 
+    audio.play(); 
+  } 
+  
+  function pauseAudio() { 
+    audio.pause(); 
+  } 
